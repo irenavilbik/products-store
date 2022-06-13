f4e9abc149205fabd666ac2627811abb62b09a41
irenavilbik <i_vilbik@yahoo.com>
2022-06-13 20:32:41 +0300

    updates

--- src/main/java/lt/bit/productsstore/controller/ProductStore.java
+++ src/main/java/lt/bit/productsstore/controller/ProductStore.java
@@ -1,16 +1,14 @@
 package lt.bit.productsstore.controller;
-
 import lt.bit.productsstore.model.Product;
-
 import java.time.LocalDate;
 import java.util.List;
 
 public class ProductStore {
-    private static final List<Product> PRODUCTS = List.of{
-        new Product (1, "product1", "Desc1", LocalDate.now());
-        new Product (1, "product2", "Desc2", LocalDate.now());
-        new Product (1, "product3", "Desc3", LocalDate.now());
-        new Product (1, "product4", "Desc4", LocalDate.now());
+    private static final List<Product> PRODUCTS = List.of(
+            new Product(1, "p1", "desc1", LocalDate.now()),
+            new Product(2, "p2", "d2", LocalDate.now().minusDays(2)),
+            new Product(3, "p3", "d3", LocalDate.now().minusMonths(1).minusDays(1))
+    );
 
     static List<Product> getProducts() {
         return PRODUCTS;
